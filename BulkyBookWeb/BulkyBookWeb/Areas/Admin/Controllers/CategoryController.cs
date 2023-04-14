b34c020de42eda9f11680b9e409e9fdf8a3663dc
Raymond Hua <17263901+raymondhua@users.noreply.github.com>
2023-04-14 17:35:33 +1200

    Completed 345: Assignment 3

--- BulkyBookWeb/BulkyBookWeb/Areas/Admin/Controllers/CategoryController.cs
+++ BulkyBookWeb/BulkyBookWeb/Areas/Admin/Controllers/CategoryController.cs
@@ -3,111 +3,111 @@ using BulkyBook.DataAccess;
 using Microsoft.AspNetCore.Mvc;
 using BulkyBook.DataAccess.Repository.IRepository;
 
-namespace BulkyBookWeb.Areas.Admin.Controllers
+namespace BulkyBookWeb.Areas.Admin.Controllers;
+
+[Area("Admin")]
+public class CategoryController : Controller
 {
-    [Area("Admin")]
-    public class CategoryController : Controller
+    private readonly IUnitOfWork _unitOfWork;
+
+    public CategoryController(IUnitOfWork unitOfWork)
     {
-        private readonly IUnitOfWork _unitOfWork;
+        _unitOfWork = unitOfWork;
+    }
 
-        public CategoryController(IUnitOfWork unitOfWork)
-        {
-            _unitOfWork = unitOfWork;
-        }
+    public IActionResult Index()
+    {
+        IEnumerable<Category> objCategoryList = _unitOfWork.Category.GetAll();
+        return View(objCategoryList);
+    }
+    //GET
+    public IActionResult Create()
+    {
 
-        public IActionResult Index()
+        return View();
+    }
+    //POST
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Create(Category obj)
+    {
+        if (obj.Name == obj.DisplayOrder.ToString())
         {
-            IEnumerable<Category> objCategoryList = _unitOfWork.Category.GetAll();
-            return View(objCategoryList);
+            ModelState.AddModelError("name", "The DisplayOrder cannot be exactly be the same");
         }
-        //GET
-        public IActionResult Create()
+        if (ModelState.IsValid)
         {
-
-            return View();
-        }
-        //POST
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public IActionResult Create(Category obj)
-        {
-            if (obj.Name == obj.DisplayOrder.ToString())
-            {
-                ModelState.AddModelError("name", "The DisplayOrder cannot be exactly be the same");
-            }
-            if (ModelState.IsValid)
-            {
-                _unitOfWork.Category.Add(obj);
-                _unitOfWork.Save();
-                TempData["success"] = "Category created successfully";
-                return RedirectToAction("Index");
-            }
-            return View(obj);
-        }
-        //GET
-        public IActionResult Edit(int? id)
-        {
-            if (id == null || id == 0)
-            {
-                return NotFound();
-            }
-            //var categoryFromDb = _unitOfWork.Category.Find(id);
-            var categoryFromDbFirst = _unitOfWork.Category.GetFirstOrDefault(u => u.Id == id);
-            //var categoryFromDbSingle = _unitOfWork.Category.SingleOrDefault(u => u.Id == id);
-            if (categoryFromDbFirst == null)
-            {
-                return NotFound();
-            }
-            return View(categoryFromDbFirst);
-        }
-        //POST
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public IActionResult Edit(Category obj)
-        {
-            if (obj.Name == obj.DisplayOrder.ToString())
-            {
-                ModelState.AddModelError("name", "The DisplayOrder cannot be exactly be the same");
-            }
-            if (ModelState.IsValid)
-            {
-                _unitOfWork.Category.Update(obj);
-                _unitOfWork.Save();
-                TempData["success"] = "Category updated successfully";
-                return RedirectToAction("Index");
-            }
-            return View(obj);
-        }
-        //GET
-        public IActionResult Delete(int? id)
-        {
-            if (id == null || id == 0)
-            {
-                return NotFound();
-            }
-            //var categoryFromDb = _db.Categories.Find(id);
-            var categoryFromDbFirst = _unitOfWork.Category.GetFirstOrDefault(u => u.Id == id);
-            //var categoryFromDbSingle = _db.Categories.SingleOrDefault(u => u.Id == id);
-            if (categoryFromDbFirst == null)
-            {
-                return NotFound();
-            }
-            return View(categoryFromDbFirst);
-        }
-        //POST
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public IActionResult DeletePOST(int id)
-        {
-            var obj = _unitOfWork.Category.GetFirstOrDefault(u => u.Id == id);
-            if (obj == null)
-            {
-                return NotFound();
-            }
-            _unitOfWork.Category.Remove(obj);
+            _unitOfWork.Category.Add(obj);
             _unitOfWork.Save();
-            TempData["success"] = "Category deleted successfully";
+            TempData["success"] = "Category created successfully";
             return RedirectToAction("Index");
         }
+        return View(obj);
+    }
+    //GET
+    public IActionResult Edit(int? id)
+    {
+        if (id == null || id == 0)
+        {
+            return NotFound();
+        }
+        //var categoryFromDb = _unitOfWork.Category.Find(id);
+        var categoryFromDbFirst = _unitOfWork.Category.GetFirstOrDefault(u => u.Id == id);
+        //var categoryFromDbSingle = _unitOfWork.Category.SingleOrDefault(u => u.Id == id);
+        if (categoryFromDbFirst == null)
+        {
+            return NotFound();
+        }
+        return View(categoryFromDbFirst);
+    }
+    //POST
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Edit(Category obj)
+    {
+        if (obj.Name == obj.DisplayOrder.ToString())
+        {
+            ModelState.AddModelError("name", "The DisplayOrder cannot be exactly be the same");
+        }
+        if (ModelState.IsValid)
+        {
+            _unitOfWork.Category.Update(obj);
+            _unitOfWork.Save();
+            TempData["success"] = "Category updated successfully";
+            return RedirectToAction("Index");
+        }
+        return View(obj);
+    }
+    //GET
+    public IActionResult Delete(int? id)
+    {
+        if (id == null || id == 0)
+        {
+            return NotFound();
+        }
+        //var categoryFromDb = _db.Categories.Find(id);
+        var categoryFromDbFirst = _unitOfWork.Category.GetFirstOrDefault(u => u.Id == id);
+        //var categoryFromDbSingle = _db.Categories.SingleOrDefault(u => u.Id == id);
+        if (categoryFromDbFirst == null)
+        {
+            return NotFound();
+        }
+        return View(categoryFromDbFirst);
+    }
+    //POST
+    [HttpPost, ActionName("Delete")]
+    [ValidateAntiForgeryToken]
+    public IActionResult DeletePOST(int id)
+    {
+        var obj = _unitOfWork.Category.GetFirstOrDefault(u => u.Id == id);
+        if (obj == null)
+        {
+            return NotFound();
+        }
+        _unitOfWork.Category.Remove(obj);
+        _unitOfWork.Save();
+        TempData["success"] = "Category deleted successfully";
+        return RedirectToAction("Index");
     }
 }
+
--- BulkyBookWeb/BulkyBookWeb/Areas/Admin/Controllers/CompanyController.cs
+++ BulkyBookWeb/BulkyBookWeb/Areas/Admin/Controllers/CompanyController.cs
@@ -1,111 +1,87 @@
 ﻿using BulkyBook.DataAccess.Repository;
 using BulkyBook.DataAccess.Repository.IRepository;
 using BulkyBook.Models;
+using BulkyBook.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.Extensions.Hosting;
 
-namespace BulkyBookWeb.Areas.Admin.Controllers
+namespace BulkyBookWeb.Areas.Admin.Controllers;
+
+[Area("Admin")]
+public class CompanyController : Controller
 {
-    [Area("Admin")]
-    public class CompanyController : Controller
+    private readonly IUnitOfWork _unitOfWork;
+    public CompanyController(IUnitOfWork unitOfWork)
     {
-        private readonly IUnitOfWork _unitOfWork;
-        public CompanyController(IUnitOfWork unitOfWork)
-        {
-            _unitOfWork = unitOfWork;
-        }
-        public IActionResult Index()
-        {
-            IEnumerable<Company> objCompanyList = _unitOfWork.Company.GetAll();
-            return View(objCompanyList);
-        }
-        //GET
-        public IActionResult Create()
-        {
+        _unitOfWork = unitOfWork;
+    }
+    public IActionResult Index()
+    {
+        IEnumerable<Company> objCompanyList = _unitOfWork.Company.GetAll();
+        return View(objCompanyList);
+    }
 
-            return View();
-        }
-        //POST
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public IActionResult Create(Company obj)
+    //GET
+    public IActionResult Upsert(int? id)
+    {
+        Company company = new();
+
+        if (id == null || id == 0)
         {
-            if (ModelState.IsValid)
+            //create company
+            return View(company);
+        }
+        else
+        {
+            //update company
+            company = _unitOfWork.Company.GetFirstOrDefault(u => u.Id == id);
+            return View(company);
+        }
+    }
+    //POST
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Upsert(Company obj)
+    {
+        if (ModelState.IsValid)
+        {
+            if (obj.Id == 0)
             {
                 _unitOfWork.Company.Add(obj);
                 _unitOfWork.Save();
                 TempData["success"] = "Company created successfully";
-                return RedirectToAction("Index");
             }
-            return View(obj);
-        }
-        //GET
-        public IActionResult Edit(int? id)
-        {
-            if (id == null || id == 0)
-            {
-                return NotFound();
-            }
-            //var companyFromDb = _unitOfWork.Company.Find(id);
-            var companyFromDbFirst = _unitOfWork.Company.GetFirstOrDefault(u => u.Id == id);
-            //var companyFromDbSingle = _unitOfWork.Company.SingleOrDefault(u => u.Id == id);
-            if (companyFromDbFirst == null)
-            {
-                return NotFound();
-            }
-            return View(companyFromDbFirst);
-        }
-        //POST
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public IActionResult Edit(Company obj)
-        {
-            if (ModelState.IsValid)
+            else
             {
                 _unitOfWork.Company.Update(obj);
                 _unitOfWork.Save();
                 TempData["success"] = "Company updated successfully";
-                return RedirectToAction("Index");
             }
-            return View(obj);
-        }
-        //GET
-        public IActionResult Delete(int? id)
-        {
-            if (id == null || id == 0)
-            {
-                return NotFound();
-            }
-            //var companyFromDb = _db.Company.Find(id);
-            var companyFromDbFirst = _unitOfWork.Company.GetFirstOrDefault(u => u.Id == id);
-            //var categoryFromDbSingle = _db.Company.SingleOrDefault(u => u.Id == id);
-            if (companyFromDbFirst == null)
-            {
-                return NotFound();
-            }
-            return View(companyFromDbFirst);
-        }
-        //POST
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public IActionResult DeletePOST(int id)
-        {
-            var obj = _unitOfWork.Company.GetFirstOrDefault(u => u.Id == id);
-            if (obj == null)
-            {
-                return NotFound();
-            }
-            _unitOfWork.Company.Remove(obj);
-            _unitOfWork.Save();
-            TempData["success"] = "Company deleted successfully";
             return RedirectToAction("Index");
         }
-        #region API CALLS
-        [HttpGet]
-        public IActionResult GetAll()
-        {
-            var companyList = _unitOfWork.Company.GetAll();
-            return Json(new { data = companyList });
-        }
-        #endregion
+        return View(obj);
     }
+    //GET
+    #region API CALLS
+    [HttpGet]
+    public IActionResult GetAll()
+    {
+        var companyList = _unitOfWork.Company.GetAll();
+        return Json(new { data = companyList });
+    }
+    //POST
+    [HttpDelete]
+    public IActionResult Delete(int? id)
+    {
+        var obj = _unitOfWork.Company.GetFirstOrDefault(u => u.Id == id);
+        if (obj == null)
+        {
+            return Json(new { success = false, message = "Error while deleting" });
+        }
+        _unitOfWork.Company.Remove(obj);
+        _unitOfWork.Save();
+        return Json(new { success = true, message = "Delete Successful" });
+    }
+    #endregion
 }
