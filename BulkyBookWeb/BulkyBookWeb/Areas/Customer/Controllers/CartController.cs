067bdbf0cfb106e238035085606a2755004252d4
Raymond Hua <17263901+raymondhua@users.noreply.github.com>
2023-05-23 15:10:27 +1200

    Completed 363

--- BulkyBookWeb/BulkyBookWeb/Areas/Customer/Controllers/CartController.cs
+++ BulkyBookWeb/BulkyBookWeb/Areas/Customer/Controllers/CartController.cs
@@ -31,13 +31,41 @@ namespace BulkyBookWeb.Areas.Customer.Controllers
             {
                 cart.Price = GetPriceBasedOnQuantity(cart.Count, cart.Product.Price, cart.Product.Price50,
                     cart.Product.Price100);
+                ShoppingCartVM.CartTotal += (cart.Price * cart.Count);
             }
             return View(ShoppingCartVM);
         }
 
+        public IActionResult Plus(int cartId)
+        {
+            var cart = _unitOfWork.ShoppingCart.GetFirstOrDefault(u => u.Id == cartId);
+            _unitOfWork.ShoppingCart.IncrementCount(cart, 1);
+            _unitOfWork.Save();
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Minus(int cartId)
+        {
+            var cart = _unitOfWork.ShoppingCart.GetFirstOrDefault(u => u.Id == cartId);
+            if (cart.Count <= 1)
+                _unitOfWork.ShoppingCart.Remove(cart);
+            else
+                _unitOfWork.ShoppingCart.DecrementCount(cart, 1);
+            _unitOfWork.Save();
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Remove(int cartId)
+        {
+            var cart = _unitOfWork.ShoppingCart.GetFirstOrDefault(u => u.Id == cartId);
+            _unitOfWork.ShoppingCart.Remove(cart);
+            _unitOfWork.Save();
+            return RedirectToAction(nameof(Index));
+        }
+
         private double GetPriceBasedOnQuantity(double quantity, double price, double price50, double price100)
         {
-            if (quantity == 50)
+            if (quantity <= 50)
                 return price;
             else
             {
