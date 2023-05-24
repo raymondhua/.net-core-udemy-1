88989106ba2e3f105704ef2e90e4d18abe890b61
Raymond Hua <17263901+raymondhua@users.noreply.github.com>
2023-05-24 14:17:21 +1200

    Created OrderHeader and OrderDetail models

--- BulkyBookWeb/BulkyBook.DataAccess/Data/ApplicationDbContext.cs
+++ BulkyBookWeb/BulkyBook.DataAccess/Data/ApplicationDbContext.cs
@@ -16,5 +16,7 @@ public class ApplicationDbContext: IdentityDbContext
     public DbSet<ApplicationUser> ApplicationUsers { get; set; }
 	public DbSet<Company> Companies { get; set; }
     public DbSet<ShoppingCart> ShoppingCarts { get; set; }
+    public DbSet<OrderHeader> OrderHeaders { get; set; }
+    public DbSet<OrderDetails> OrderDetails { get; set; }
 }
 
