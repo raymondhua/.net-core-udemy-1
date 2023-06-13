6271ea9695c467900179109c28cd9ca92ae78d35
Raymond Hua <17263901+raymondhua@users.noreply.github.com>
2023-06-13 13:09:25 +1200

    Added Details in OrderController

--- BulkyBookWeb/BulkyBookWeb/Areas/Admin/Controllers/OrderController.cs
+++ BulkyBookWeb/BulkyBookWeb/Areas/Admin/Controllers/OrderController.cs
@@ -3,6 +3,7 @@ using System.Diagnostics;
 using System.Security.Claims;
 using BulkyBook.DataAccess.Repository.IRepository;
 using BulkyBook.Models;
+using BulkyBook.Models.ViewModels;
 using BulkyBook.Utility;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -14,6 +15,8 @@ namespace BulkyBookWeb.Areas.Admin.Controllers
     public class OrderController : Controller
     {
         public readonly IUnitOfWork _unitOfWork;
+        [BindProperty]
+        public OrderVM OrderVM { get; set; }
 
         public OrderController(IUnitOfWork unitOfWork)
         {
@@ -23,15 +26,22 @@ namespace BulkyBookWeb.Areas.Admin.Controllers
         {
             return View();
         }
+        public IActionResult Details(int orderId)
+        {
+            OrderVM = new OrderVM()
+            {
+                OrderHeader = _unitOfWork.OrderHeader.GetFirstOrDefault(u=> u.Id==orderId, includeProperties:"ApplicationUser"),
+                OrderDetail = _unitOfWork.OrderDetail.GetAll(u => u.OrderId == orderId, includeProperties: "Product"),
+            };
+            return View(OrderVM);
+        }
         #region API CALLS
         [HttpGet]
         public IActionResult GetAll(string status)
         {
             IEnumerable<OrderHeader> orderHeaders; 
             if (User.IsInRole(SD.Role_Admin) || User.IsInRole(SD.Role_Employee))
-            {
                 orderHeaders = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser");
-            }
             else
             {
                 var claimsIdentity = (ClaimsIdentity)User.Identity;
