4baf81ad60ada8b7d1fb52f52956500736da81c6
Raymond Hua <17263901+raymondhua@users.noreply.github.com>
2023-07-04 14:50:22 +1200

    Rearranged program.cs

--- BulkyBookWeb/BulkyBookWeb/Program.cs
+++ BulkyBookWeb/BulkyBookWeb/Program.cs
@@ -9,73 +9,109 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
 using Stripe;
 
-var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddControllersWithViews();
-builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(
-    builder.Configuration.GetConnectionString("DefaultConnection"), options => options.MigrationsAssembly("BulkyBook.DataAccess")
-    ));
-builder.Services.Configure<StripeSettings>(builder.Configuration.GetSection("Stripe"));
-builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddDefaultTokenProviders()
-    .AddEntityFrameworkStores<ApplicationDbContext>();
-builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
-builder.Services.AddScoped<IDbInitializer, DbInitializer>();
-builder.Services.AddSingleton<IEmailSender, EmailSender>();
-builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
-builder.Services.AddAuthentication().AddFacebook(options =>
+using Serilog;
+using BulkyBook.CloudStorage.Common;
+using BulkyBook.CloudStorage.Repository;
+using BulkyBook.CloudStorage.Service;
+
+StaticLogger.EnsureInitialized();
+Log.Information("Azure Storage API Booting Up...");
+
+try
 {
-    options.AppId = builder.Configuration.GetValue<string>("FacebookSettings:AppId");
-    options.AppSecret = builder.Configuration.GetValue<string>("FacebookSettings:AppSecret");
-});
-builder.Services.ConfigureApplicationCookie(options => 
-{ 
-    options.LoginPath = $"/Identity/Account/Login";
-    options.LogoutPath = $"/Identity/Account/Logout";
-    options.AccessDeniedPath = $"/Identity/Account/AccessDenied";
-}
-);
-builder.Services.AddDistributedMemoryCache();
-builder.Services.AddSession(options => {
-    options.IdleTimeout = TimeSpan.FromMinutes(100);
-    options.Cookie.HttpOnly = true;
-    options.Cookie.IsEssential = true;
-});
-var app = builder.Build();
+    var builder = WebApplication.CreateBuilder(args);
+
+    builder.Host.UseSerilog((_, config) =>
+    {
+        config.WriteTo.Console()
+            .WriteTo.Seq(serverUrl: "http://seq:5341")
+            .ReadFrom.Configuration(builder.Configuration);
+    });
+
+    // Add services to the container.
+    builder.Services.AddControllersWithViews();
+    builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(
+        builder.Configuration.GetConnectionString("DefaultConnection"),
+        options => options.MigrationsAssembly("BulkyBook.DataAccess")
+    ));
+    builder.Services.Configure<StripeSettings>(builder.Configuration.GetSection("Stripe"));
+    builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddDefaultTokenProviders()
+        .AddEntityFrameworkStores<ApplicationDbContext>();
+    builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+    builder.Services.AddScoped<IDbInitializer, DbInitializer>();
+    builder.Services.AddSingleton<IEmailSender, EmailSender>();
+    builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
+    builder.Services.AddAuthentication().AddFacebook(options =>
+    {
+        options.AppId = builder.Configuration.GetValue<string>("FacebookSettings:AppId");
+        options.AppSecret = builder.Configuration.GetValue<string>("FacebookSettings:AppSecret");
+    });
+    builder.Services.ConfigureApplicationCookie(options =>
+        {
+            options.LoginPath = $"/Identity/Account/Login";
+            options.LogoutPath = $"/Identity/Account/Logout";
+            options.AccessDeniedPath = $"/Identity/Account/AccessDenied";
+        }
+    );
+    builder.Services.AddDistributedMemoryCache();
+    builder.Services.AddSession(options =>
+    {
+        options.IdleTimeout = TimeSpan.FromMinutes(100);
+        options.Cookie.HttpOnly = true;
+        options.Cookie.IsEssential = true;
+    });
+
+    // Add Azure Repository Service
+    builder.Services.AddTransient<IAzureStorage, AzureStorage>();
+    Log.Information("Services has been successfully added...");
+
+    var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-StripeConfiguration.ApiKey = builder.Configuration.GetSection("Stripe:SecretKey").Get<string>();
-SeedDatabase();
-app.UseAuthentication();
-
-app.UseAuthorization();
-app.UseSession();
-app.MapRazorPages();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{area=Customer}/{controller=Home}/{action=Index}/{id?}");
-
-app.Run();
-
-void SeedDatabase()
-{
-    using (var scope = app.Services.CreateScope())
+    if (!app.Environment.IsDevelopment())
     {
-        var dbInitializer = scope.ServiceProvider.GetRequiredService<IDbInitializer>();
-        dbInitializer.Initialize();
+        app.UseExceptionHandler("/Home/Error");
+        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+        app.UseHsts();
+    }
+
+    app.UseHttpsRedirection();
+    app.UseStaticFiles();
+
+    app.UseRouting();
+
+    StripeConfiguration.ApiKey = builder.Configuration.GetSection("Stripe:SecretKey").Get<string>();
+    SeedDatabase();
+    app.UseAuthentication();
+
+    app.UseAuthorization();
+    app.UseSession();
+    app.MapRazorPages();
+
+    app.MapControllerRoute(
+        name: "default",
+        pattern: "{area=Customer}/{controller=Home}/{action=Index}/{id?}");
+
+    app.Run();
+
+    void SeedDatabase()
+    {
+        using (var scope = app.Services.CreateScope())
+        {
+            var dbInitializer = scope.ServiceProvider.GetRequiredService<IDbInitializer>();
+            dbInitializer.Initialize();
+        }
     }
 }
-
+catch (Exception ex) when (!ex.GetType().Name.Equals("StopTheHostException", StringComparison.Ordinal))
+{
+    StaticLogger.EnsureInitialized();
+    Log.Fatal(ex, "Unhandled Exception");
+}
+finally
+{
+    StaticLogger.EnsureInitialized();
+    Log.Information("Azure Storage API Shutting Down...");
+    Log.CloseAndFlush();
+}
